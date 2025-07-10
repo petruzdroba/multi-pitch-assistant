5b3713beb8752355c6f14fd0dbeb87b09c0f134a
Zdroba Petru <petru.zdroba@gmail.com>
2025-07-10 16:11:25 +0300

    base component

---
+++ src/app/components/session/session.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-session',
+  templateUrl: './session.component.html',
+  styleUrls: ['./session.component.css'],
+})
+export class SessionComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
