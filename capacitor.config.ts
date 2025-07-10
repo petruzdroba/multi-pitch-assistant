19d789d27bc6ade07e700284489b172dc26eb27b
Zdroba Petru <petru.zdroba@gmail.com>
2025-07-10 15:21:10 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'multi-buddy',
+  webDir: 'www'
+};
+
+export default config;
