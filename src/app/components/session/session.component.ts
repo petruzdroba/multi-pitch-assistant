71cd9b11683af0c8c8f9e20043a302c8837719a3
Zdroba Petru <petru.zdroba@gmail.com>
2025-07-17 14:27:27 +0300

    added confirm dialog on end session

--- src/app/components/session/session.component.ts
+++ src/app/components/session/session.component.ts
@@ -5,6 +5,7 @@ import { IonButton, IonList, IonItem } from '@ionic/angular/standalone';
 import { CanComponentDeactivate } from 'src/app/guards/session-exit.guard';
 import { SessionService } from 'src/app/services/session.service';
 import { AlertController } from '@ionic/angular';
+import { Dialog } from '@capacitor/dialog';
 
 @Component({
   selector: 'app-session',
@@ -12,10 +13,11 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./session.component.css'],
   imports: [IonItem, IonList, IonButton, DatePipe, CommonModule],
 })
-export class SessionComponent {
+export class SessionComponent implements CanComponentDeactivate {
   private sessionService = inject(SessionService);
   private routerService = inject(Router);
   private sessionEnded = false;
+  private alertCtrl = inject(AlertController);
 
   canDeactivate(): boolean {
     return this.sessionEnded;
@@ -35,12 +37,19 @@ export class SessionComponent {
     });
   }
 
-  onEndSession() {
-    console.log('Ending session...');
-    this.sessionService.endSession();
+  async onEndSession() {
+    const { value } = await Dialog.confirm({
+      title: 'Confirm End',
+      message: 'Are you sure you want to end the session?',
+    });
 
-    setTimeout(() => {
-      this.routerService.navigate(['/tabs/home']);
-    }, 100);
+    if (value) {
+      console.log('Confirmed: Ending session...');
+      this.sessionEnded = true;
+      this.sessionService.endSession();
+      setTimeout(() => {
+        this.routerService.navigate(['/tabs/home']);
+      }, 100);
+    }
   }
 }
