764ce79f0db44a140a0f48239f6d5c6d22bafdd2
Zdroba Petru <petru.zdroba@gmail.com>
2025-07-22 11:46:45 +0300

    load session details + cancel button

--- src/app/components/session-details/session-details.component.ts
+++ src/app/components/session-details/session-details.component.ts
@@ -1,15 +1,19 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Session } from 'src/app/models/session.interface';
 import { LogService } from 'src/app/services/log.service';
+import { IonProgressBar, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-session-details',
   templateUrl: './session-details.component.html',
   styleUrls: ['./session-details.component.css'],
+  imports: [IonProgressBar, IonButton],
 })
 export class SessionDetailsComponent implements OnInit {
   private logService = inject(LogService);
+  private routerService = inject(Router);
+
   loadedSession = signal<Session | undefined>(undefined);
   constructor(private route: ActivatedRoute) {}
 
@@ -22,5 +26,9 @@ export class SessionDetailsComponent implements OnInit {
         console.error('Session not found:', sessionId);
       }
     });
+  }
+
+  onCancel() {
+    this.routerService.navigate(['/tabs/log'], { replaceUrl: true });
   }
 }
