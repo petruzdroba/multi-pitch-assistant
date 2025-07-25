6d8c84d4d9a6b5c8c470784cd70cea21b4f7f401
Zdroba Petru <petru.zdroba@gmail.com>
2025-07-25 14:55:58 +0300

    open modal

--- src/app/components/session-details/session-details.component.ts
+++ src/app/components/session-details/session-details.component.ts
@@ -14,21 +14,27 @@ import {
   IonItemSliding,
   IonItemOptions,
   IonItemOption,
+  ModalController,
 } from '@ionic/angular/standalone';
 import { CommonModule, DatePipe } from '@angular/common';
+import { ClimbEvent } from 'src/app/models/climb-event.interface';
+import { EditEventModalComponent } from './edit-event-modal/edit-event-modal.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-session-details',
   templateUrl: './session-details.component.html',
   styleUrls: ['./session-details.component.css'],
   imports: [
+    DatePipe,
+    CommonModule,
+    FormsModule,
+
     IonProgressBar,
     IonButton,
-    IonInput,
     IonItem,
-    DatePipe,
+    IonInput,
     IonList,
-    CommonModule,
     IonFab,
     IonFabButton,
     IonIcon,
@@ -40,9 +46,24 @@ import { CommonModule, DatePipe } from '@angular/common';
 export class SessionDetailsComponent implements OnInit {
   private logService = inject(LogService);
   private routerService = inject(Router);
+  private modalController = inject(ModalController);
 
   loadedSession = signal<Session | undefined>(undefined);
   constructor(private route: ActivatedRoute) {}
+
+  async openModal(event: ClimbEvent | undefined = undefined) {
+    const modal = await this.modalController.create({
+      component: EditEventModalComponent,
+      componentProps: { event: event },
+    });
+    modal.present();
+    const { data, role } = await modal.onWillDismiss();
+
+    if (role === 'confirm') {
+      // this.message = `Hello, ${data}!`;
+      console.log('modal data:', data);
+    }
+  }
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
@@ -57,4 +78,8 @@ export class SessionDetailsComponent implements OnInit {
   onCancel() {
     this.routerService.navigate(['/tabs/log'], { replaceUrl: true });
   }
+
+  onEdit(event: ClimbEvent) {}
+
+  onDelete(event: ClimbEvent) {}
 }
