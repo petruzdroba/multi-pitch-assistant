90a12ee9990563d2b7a643f06608911694fd788b
Petru Zdroba <petru.zdroba@gmail.com>
2025-10-06 13:30:14 +0300

    feat: implement AuthService with login, logout, and user retrieval functionality

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,54 @@
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { UserData } from '../models/user-data.interface';
+import { map, Observable, take, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.test';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private http = inject(HttpClient);
+  private userData = signal<UserData | null>(null);
+
+  public readonly user = computed(() => this.userData());
+  public readonly isLoggedIn = computed(() => this.userData() !== null);
+
+  logIn(data: { email: string; password: string }): Observable<UserData> {
+    return this.http
+      .post<{ access: string; refresh: string; user: UserData }>(
+        `${environment.serverUrl}/login/`,
+        data
+      )
+      .pipe(
+        take(1),
+        tap(async (res) => {
+          await Storage.set({ key: 'accessToken', value: res.access });
+          await Storage.set({ key: 'refreshToken', value: res.refresh });
+          this.userData.set(res.user);
+        }),
+        map((res) => res.user)
+      );
+  }
+
+  async logOut(): Promise<void> {
+    await Storage.remove({ key: 'accessToken' });
+    await Storage.remove({ key: 'refreshToken' });
+    this.userData.set(null);
+  }
+
+  checkRememberedUser(): void {
+    Storage.get({ key: 'accessToken' }).then(({ value: token }) => {
+      if (token) {
+        this.http
+          .get<UserData>(`${environment.serverUrl}/auth/me/`, {
+            headers: { Authorization: `Bearer ${token}` },
+          })
+          .pipe(take(1))
+          .subscribe({
+            next: (user) => this.userData.set(user),
+            error: () => this.logOut(),
+          });
+      }
+    });
+  }
+}
