4eac1bbc24f495a0ef5fb3bd30ffc4b701799fea
Petru Zdroba <petru.zdroba@gmail.com>
2025-10-08 10:30:34 +0300

    refactor: made layout look better

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { IonToolbar, IonSegment, IonSegmentButton, IonLabel, IonSegmentView, IonSegmentContent } from "@ionic/angular/standalone";
+import { LoginFormComponent } from "./login-form/login-form.component";
+import { SignupFormComponent } from "./signup-form/signup-form.component";
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css'],
-  imports: [IonToolbar, IonSegment, IonSegmentButton, IonLabel, IonSegmentView, IonSegmentContent],
+  imports: [IonToolbar, IonSegment, IonSegmentButton, IonLabel, IonSegmentView, IonSegmentContent, LoginFormComponent, SignupFormComponent],
 })
 export class UserProfileComponent {
   private authService = inject(AuthService)
