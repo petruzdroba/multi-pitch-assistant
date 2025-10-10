e8de66647b53a62d3ae2ead041f313d527149c60
Petru Zdroba <petru.zdroba@gmail.com>
2025-10-10 13:56:41 +0300

    feat: added log out button

--- src/app/components/user-profile/user-data/user-data.component.ts
+++ src/app/components/user-profile/user-data/user-data.component.ts
@@ -1,26 +1,28 @@
 import { AuthService } from 'src/app/services/auth.service';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { UserData } from 'src/app/models/user-data.interface';
-import { IonItem, IonLabel, IonInput } from "@ionic/angular/standalone";
+import { IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-data',
   templateUrl: './user-data.component.html',
   styleUrls: ['./user-data.component.css'],
-  imports: [IonItem, IonLabel, IonInput]
+  imports: [IonItem, IonLabel, IonInput],
 })
 export class UserDataComponent implements OnInit {
   private authService = inject(AuthService);
   protected userData = signal<UserData>({} as UserData);
+  private router = inject(Router);
 
   ngOnInit() {
-    this.userData.set(this.authService.user() || {} as UserData);
+    this.userData.set(this.authService.user() || ({} as UserData));
   }
 
+  onDelete() {}
 
-  onDelete(){}
-
-  onLogout(){
-
+  onLogout() {
+    this.authService.logOut();
+    this.router.navigate(['/tabs/home']);
   }
 }
